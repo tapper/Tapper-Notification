ec8c4c8e058db4b9b57302cd9c72fca1aa1139cc
Maik Hentsche <maik.hentsche@amd.com>
2012-02-13 13:49:42 +0100

    [FEAT] implement advanced notification conditions
    This new feature brings the function testrun_success_change. This
    function allows to check if the last given number of testruns with a
    given condition have a different success state than the one that
    generated the event.

--- lib/Tapper/Notification.pm
+++ lib/Tapper/Notification.pm
@@ -11,8 +11,8 @@ use Try::Tiny;
 use Hash::Merge::Simple 'merge';
 use Language::Expr;
 
-use Data::DPath 'dpath';
-use Tapper::Reports::DPath;
+use Data::DPath qw/dpath/;
+use Tapper::Reports::DPath qw/reportdata /;
 
 
 extends 'Tapper::Base';
@@ -20,6 +20,11 @@ extends 'Tapper::Base';
 has cfg => (is => 'rw', default => sub { Tapper::Config->subconfig} );
 
 our $VERSION = '3.000001';
+
+# We have these variables global to have them available in condition
+# match functions in notification subscriptions. If they were not global
+# the functions used in these conditions would need to have them in the API
+our ($testrun, $report);
 
 =head1 NAME
 
@@ -113,7 +118,61 @@ sub get_report_data
         return $report_hash;
 }
 
+=head2 get_testrun_success
 
+Get the overall success of the testrun with given id. If an error
+occurs, the function returns undef. This is most probably because the
+given testrun has no reports and thus no testrun stats can be found.
+
+@param int - testrun id
+
+@return success - success rate in percent
+@return error   - undef
+
+=cut
+
+sub get_testrun_success
+{
+        my ($testrun_id) = @_;
+        my $stats = model('ReportsDB')->resultset('ReportgroupTestrunStats')->search({testrun_id => $testrun_id});
+        return undef if not $stats->count;
+        return ($stats->first->success_ratio == 100) ? 'pass' : 'fail';
+}
+
+=head2 testrun_success_change
+
+Return whether the last given testruns of a given condition have a
+different success state as the current one.
+
+@param string - search string to define matching testruns
+@param int    - number of testruns to look back
+
+@return boolean - success change (yes/no)?
+
+=cut
+
+sub testrun_success_change
+{
+        my ($search, $lookback) = @_;
+        my $testruns = model('TestrunDB')->resultset('Testrun')->search($search);
+        my $success;
+        while ($lookback-- and my $this_testrun = $testruns->next) {
+                # the testrun that triggered this check is not part of the backlog since we want
+                # to check its success against the success of the testruns before this one
+                do {$lookback++, next} if $this_testrun->id == $testrun->{id};
+                my $this_success = get_testrun_success($this_testrun->id);
+                # this testrun has no reports, ignore it and don't count it against lookback
+                do {$lookback++, next} if not defined $this_success;
+
+                $success = $this_success if not defined($success);
+                # last $lookback tests did not have the same success state
+                return 0 if not $this_success ~~ $success;
+        }
+        my $testrun_success = get_testrun_success($testrun->{id});
+
+        return 1 if defined($testrun_success) and defined($success) and $testrun_success ne $success;
+        return 0;
+}
 
 =head2 matches
 
@@ -130,23 +189,15 @@ event, i.e. whether we need to notify the user.
 sub matches
 {
         my ($self, $condition, $event) = @_;
-        our ($testrun, $all_testruns, $testrun_reportgroup, $report, $all_reports);
         given($event->type){
                 when ('testrun_finished') {
                         $testrun             = $self->get_testrun_data($event->message->{testrun_id});
-                        $all_testruns        = model('TestrunDB')->resultset('Testrun');
-                        $testrun_reportgroup = model('ReportsDB')->resultset('ReportgroupTestrun')->search({testrun_id => $event->message->{testrun_id}});
-                        $all_reports         = model('ReportsDB')->resultset('Report');
                 }
                 when ('report_received')  {
-                        $all_testruns = model('TestrunDB')->resultset('Testrun');
                         $report       = $self->get_report_data($event->message->{report_id});
-                        $all_reports  = model('ReportsDB')->resultset('Report');
                 };
                 default { return };
         }
-        *{Tapper::Notification::testrun} = sub { return ( @_ ? $testrun->{$_[0]} : $testrun ) if $testrun };
-        *{Tapper::Notification::report}  = sub { return ( @_ ? $report->{$_[0]}  : $report  ) if $report  };
 
         my $le = Language::Expr->new;
         $le->interpreted(1);
@@ -155,8 +206,20 @@ sub matches
         $le->func(testrun     => sub { return ( @_ ? $testrun->{$_[0]} : $testrun ) if $testrun });
         $le->func(report      => sub { return ( @_ ? $report->{$_[0]}  : $report  ) if $report  });
         $le->func(deep_search => sub { return dpath($_[1])->match($_[0]) } );
+        $le->func(reportdata  => sub { return reportdata @_ } );
+        $le->func(testrun_success_change  => sub { testrun_success_change @_ });
 
-        my $success = $le->eval($condition);
+        my $success;
+
+        # # FIXME: testrun_success_change triggers a bug in perl. As soon as this bug is fixed we can switch
+        # # to using Language::Expr for all functions
+        # if ($condition =~ m/^testrun_success_change(.+)/) {
+        #         $success = eval "testrun_success_change($1)";
+        # } else {
+        #         $success = $le->eval($condition);
+        # }
+        $success = $le->eval($condition);
+
         return  $success;
 }
 
@@ -189,11 +252,11 @@ sub notify_user
                 $self->log->error( "Could not load $plugin_class: $@" );
         } else {
                 try{
-                        no strict 'refs'; ## no critic
-                        $self->log->info("Call ${plugin_class}::notify()");
-                        my $cfg = $self->cfg->{notification}{sender}{plugins}{$plugin};
-                        my $obj = ${plugin_class}->new({cfg => $cfg});
-                        $obj->notify($contact->address, $text);
+                no strict 'refs'; ## no critic
+                $self->log->info("Call ${plugin_class}::notify()");
+                my $cfg = $self->cfg->{notification}{sender}{plugins}{$plugin};
+                my $obj = ${plugin_class}->new({cfg => $cfg});
+                $obj->notify($contact->address, $text);
                 } catch {
                         $self->log->error("Error occured: $_");
                 }
